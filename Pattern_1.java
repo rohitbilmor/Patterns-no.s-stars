f8b6852ea1cf6d6bdd4e79639fc3a36d6567319a
Bandela Rohith Kumar <rohitbilmore1234@gmail.com>
2021-01-20 23:46:24 +0530

    Create Pattern_1.java

---
+++ Pattern_1.java
@@ -0,0 +1,16 @@
+package com.src1;
+
+public class Pattern_1 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++) {
+      System.out.print("* ");
+			}
+			
+			System.out.println("");
+		}
+
+											}		
+							}
